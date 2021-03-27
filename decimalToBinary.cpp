cc2f69efaf14f3ca493b6f83f14e5488c0a03665
Fahim Shahriar Eram <78979590+fasher7@users.noreply.github.com>
2021-03-27 15:35:01 +0600

    Create decimalToBinary.cpp

---
+++ decimalToBinary.cpp
@@ -0,0 +1,15 @@
+int decimalToBinary (int inte)
+{
+    int bin=0, mode=1;
+    while(inte>0)
+    {
+        bin = bin + (inte%2)*mode;
+        inte = inte/2;
+        mode = mode * 10;
+    }
+    return bin;
+}
+
+/*
+decimalToBinary(43) should return 101011
+*/
