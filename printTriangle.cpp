3cac2bc6799321e815f0586fd037ca799da6cf8a
Fahim Shahriar Eram <78979590+fasher7@users.noreply.github.com>
2021-03-27 15:22:11 +0600

    Create printTriangle.cpp

---
+++ printTriangle.cpp
@@ -0,0 +1,20 @@
+void printTriangle()
+{
+   for (int i=1; i<=6; i++)
+    {
+        for (int j=1; j<=i; j++)
+        {
+            cout << "#";
+        }
+        cout << endl;
+    }
+}
+
+/*
+#
+##
+###
+####
+#####
+######
+*/
