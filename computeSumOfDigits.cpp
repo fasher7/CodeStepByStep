e1e248fb7b99e85081407bdfbdfa54fda3ec25ce
Fahim Shahriar Eram <78979590+fasher7@users.noreply.github.com>
2021-03-27 16:07:13 +0600

    Create computeSumOfDigits.cpp

---
+++ computeSumOfDigits.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int val, sum=0;
+    cout <<"Type an integer: ";
+    cin >> val;
+    while (val > 0)
+    {
+        int x = val % 10;
+        sum = sum + x;
+        val = val / 10;
+    }
+    cout << "Digit sum is " << sum;
+}
+
+/*
+Type an integer: 827104
+Digit sum is 22
+*/
